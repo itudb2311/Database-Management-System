753f40434ab95020c5ae54a37ece8f86b6a079f4
Mehmet Ali Balıkçı <balikci20@itu.edu.tr>
2023-12-21 14:51:23 +0300

    Update app.py
    Optimization

--- app.py
+++ app.py
@@ -6,8 +6,33 @@ from utils.table_operations import DistributionCenters
 
 app = Flask(__name__)
 
-connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
+def get_mysql_data_types(column_type):
+    # MySQL veri tipleri için eşleştirmeleri tanımla
+    mysql_types = {
+        246: 'DECIMAL',
+        3: 'INT',
+        12: 'DATETIME',
+        253: 'VARCHAR'
+        # Diğer veri tipleri için gerekli eşleştirmeleri ekle
+    }
+    
+    # Veri tipini döndür
+    return mysql_types.get(column_type, 'UNKNOWN')
 
+def get_table_data(table_name):
+    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)    
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    columns = cursor.description  # Sütunların adları ve tipleri
+    centers = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    for column in columns: # Sütunların tiplerini yazdırıyor
+        column_name = column[0]
+        column_type = column[1]
+        mysql_data_type = get_mysql_data_types(column_type)
+        print(f"{column_name}, {mysql_data_type}")
+    return centers
     
 @app.route('/')
 def index():
@@ -17,124 +42,53 @@ def index():
 def about():
     return render_template('about.html')
 
-def get_distribution_centers():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM distribution_centers")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 @app.route('/distribution_centers')
 def distribution_centers():
-    centers = get_distribution_centers()
+    centers = get_table_data('distribution_centers')
     return render_template('distribution_centers.html', centers=centers)
-
-def get_users():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/users')
 def users():
-    centers = get_users()
+    centers = get_table_data('users')
     return render_template('users.html', centers=centers)
-
-def get_events():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/events')
 def events():
-    centers = get_events()
+    centers = get_table_data('events')
     return render_template('events.html', centers=centers)
-
-def get_inventory_items():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM inventory_items")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/inventory_items')
 def inventory_items():
-    centers = get_inventory_items()
+    centers = get_table_data('inventory_items')
     return render_template('inventory_items.html', centers=centers)
-
-def get_products():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM products")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/products')
 def products():
-    centers = get_products()
+    centers = get_table_data('products')
     return render_template('products.html', centers=centers)
-
-def get_orders():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM orders")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/orders')
 def orders():
-    centers = get_orders()
+    centers = get_table_data('orders')
     return render_template('orders.html', centers=centers)
-
-def get_order_items():
-    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM order_items")
-    centers = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return centers
-
 
 @app.route('/order_items')
 def order_items():
-    centers = get_order_items()
+    centers = get_table_data('order_items')
     return render_template('order_items.html', centers=centers)
-
-
 
 @app.route('/search', methods=['POST'])
 def search():
     table_name = request.form['distribution_centers']
     query = request.form['searchQuery']
-
+    connection = mysql.connector.connect(host=db_host, database=db_name, user=db_user, password=db_password) 
     tables = {"distribution_centers": DistributionCenters(connection=connection)}
     table = tables[table_name]
     results = table.search(query)
     if not results:
         results = [['!','!','Could not found any data','!']]
     return render_template(f'{table_name}.html', centers=results)
-
+    connection.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
