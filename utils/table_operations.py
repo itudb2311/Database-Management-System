419de9ebb11ac2717b91e53ec45b2f74b55a6fb0
M. Serdar NAZLI <nserdarnazli@gmail.com>
2023-12-22 03:23:57 +0300

    Bug-fix of update operation

--- utils/table_operations.py
+++ utils/table_operations.py
@@ -234,10 +234,18 @@ class OrderItems:
     def update_data(self, data, id):
         try:
             cursor = self.connection.cursor()
-            cursor.execute("UPDATE order_items SET order_id=%s, user_id=%s, product_id=%s, inventory_item_id=%s, status=%s, created_at=%s, shipped_at=%s, delivered_at=%s, returned_at=%s, sale_price=%s WHERE id=%s" , (data["order_id"], data["user_id"], data["product_id"], data["inventory_item_id"], data["status"], data["created_at"], data["shipped_at"], data["delivered_at"], data["returned_at"],data["sale_price"], id))
+
+            processed_data = [
+                None if data[field] == 'None' else data[field] for field in self.columns if field in data
+            ]
+
+            update_fields = ', '.join([f"{field} = %s" for field in self.columns if field in data])
+            update_query = f"UPDATE order_items SET {update_fields} WHERE id = %s"
+
+            cursor.execute(update_query, processed_data + [id])
             self.connection.commit()
             cursor.close()
-            print("Updated", data)
+            print("Updated", processed_data)
         except Exception as e:
             print("Error while updating order_items", e)
             return f"Error: {e}"
