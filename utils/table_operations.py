2ae754684f7774d1f4a3109477dcc77bd42001da
Mehmet Ali Balıkçı <balikci20@itu.edu.tr>
2023-12-24 19:29:06 +0300

    Update products.html
    I added boxes and buttons for search, delete, insert operations, did some edits to make view better

--- utils/table_operations.py
+++ utils/table_operations.py
@@ -530,14 +530,32 @@ class Products:
         except Exception as e:
             print("Error while generating primary key for products", e)
             return f"Error: {e}"
-        
+
     def insert_data(self, data):
         try:
             cursor = self.connection.cursor()
-            cursor.execute("INSERT INTO products VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", data)
+
+            data['id'] = self.generate_primary_key()
+
+            # Convert 'None' strings to Python None and format dates
+            for key, value in data.items():
+                if value == 'None' or value == '':
+                    data[key] = None
+                # Add additional formatting as necessary, e.g., for dates
+
+            # Prepare the insert statement
+            insert_fields = ', '.join(self.columns)
+            placeholders = ', '.join(['%s' for _ in self.columns])
+            insert_query = f"INSERT INTO products ({insert_fields}) VALUES ({placeholders})"
+
+            # Extract the values in the order of self.columns
+            insert_values = [data.get(col) for col in self.columns]
+
+            # Execute the query
+            cursor.execute(insert_query, tuple(insert_values))
             self.connection.commit()
             cursor.close()
-            print("Inserted", data)
+            print("Inserted", insert_values)
         except Exception as e:
             print("Error while inserting into products", e)
             return f"Error: {e}"
@@ -545,10 +563,15 @@ class Products:
     def update_data(self, data, id):
         try:
             cursor = self.connection.cursor()
-            cursor.execute("UPDATE products SET id=%s, cost=%s, category=%s, name=%s, brand=%s, retail_price=%s, department=%s, sku=%s, distribution_center_id=%s WHERE id=%s", (*data, id))
+            processed_data = [
+                None if data[field] == 'None' else data[field] for field in self.columns if field in data
+            ]
+            update_fields = ', '.join([f"{field} = %s" for field in self.columns if field in data])
+            update_query = f"UPDATE products SET {update_fields} WHERE id = %s"
+            cursor.execute(update_query, processed_data + [id])
             self.connection.commit()
             cursor.close()
-            print("Updated", data)
+            print("Updated", processed_data)
         except Exception as e:
             print("Error while updating products", e)
             return f"Error: {e}"
@@ -564,19 +587,35 @@ class Products:
             print("Error while deleting from products", e)
             return f"Error: {e}"
 
-    def search_products(self, data):
+    def search(self, data):
         try:
             cursor = self.connection.cursor()
-            query = "SELECT * FROM products WHERE id LIKE %s AND cost LIKE %s AND category LIKE %s AND name LIKE %s AND brand LIKE %s AND retail_price LIKE %s AND department LIKE %s AND sku LIKE %s AND distribution_center_id LIKE %s"
-            parameters = tuple(data[_] + '%' for _ in range(9))
-            cursor.execute(query, parameters)
+            query_parts = []
+            parameters = []
+            for column in self.columns:
+                value = data.get(column, '') + '%'
+                if value == '%':
+                    query_parts.append(f"({column} LIKE %s OR {column} IS NULL)")
+                else:
+                    query_parts.append(f"{column} LIKE %s")
+                parameters.append(value)
+            query = "SELECT * FROM products WHERE " + " AND ".join(query_parts)
+            cursor.execute(query, tuple(parameters))
             results = cursor.fetchall()
             cursor.close()
-            return results
+            columns = cursor.description
+            column_types = []
+            for column in columns:
+                column_name = column[0]
+                column_type = column[1]
+                mysql_data_type = get_mysql_data_types(column_type)
+                item = {'column_name': column_name, 'column_type': mysql_data_type}
+                column_types.append(item)
+            return results, column_types
+        
         except Exception as e:
-            print("Could not find any corresponding value")
+            print("Could not find any corresponding value", e)
             return False
-
 
 
 
